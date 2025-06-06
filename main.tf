8c16c297243fb61ad5c2963be4c46316b6f6bfe6
Nidhi0152 <194421451+Nidhi0152@users.noreply.github.com>
2025-06-05 21:16:27 -0400

    Updating main.tf with ssh

--- main.tf
+++ main.tf
@@ -118,7 +118,7 @@ resource "azurerm_linux_virtual_machine" "main" {
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("/home/nidhi/.ssh/id_rsa.pub")
   }
 
   os_disk {
