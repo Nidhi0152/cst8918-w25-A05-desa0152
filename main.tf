562e3d0c5f7d1acc11886e0a1d11ae067a827b27
Nidhi0152 <194421451+Nidhi0152@users.noreply.github.com>
2025-06-05 21:06:02 -0400

    Adding Virtual Machine

--- main.tf
+++ main.tf
@@ -105,3 +105,34 @@ data "cloudinit_config" "config" {
     content      = file("${path.module}/init.sh")
   }
 }
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                = "${var.labelPrefix}-A05-vm"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "${var.labelPrefix}-osdisk"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  custom_data = data.cloudinit_config.config.rendered
+}
