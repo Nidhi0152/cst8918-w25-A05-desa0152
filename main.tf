293f5c1f27880856da9be3c6fcd410654cd7bada
Nidhi0152 <194421451+Nidhi0152@users.noreply.github.com>
2025-06-05 20:58:33 -0400

    Adding NIC

--- main.tf
+++ main.tf
@@ -80,3 +80,16 @@ resource "azurerm_network_security_group" "main" {
     destination_address_prefix = "*"
   }
 }
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.labelPrefix}-A05-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
