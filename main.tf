e1395ba4f3332afe2f20d9c533f21a2afe46cbc4
Nidhi0152 <194421451+Nidhi0152@users.noreply.github.com>
2025-06-05 20:59:33 -0400

    Associate NSG to NIC

--- main.tf
+++ main.tf
@@ -93,3 +93,8 @@ resource "azurerm_network_interface" "main" {
     public_ip_address_id          = azurerm_public_ip.main.id
   }
 }
+
+resource "azurerm_network_interface_security_group_association" "main" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
