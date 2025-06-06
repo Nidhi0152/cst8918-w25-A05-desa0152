ae666f705dbd7ac9d3591dbdecb07c18bd85577d
Nidhi0152 <194421451+Nidhi0152@users.noreply.github.com>
2025-06-05 20:55:36 -0400

    Adding Azure virtual network (VNet) with a CIDR range

--- main.tf
+++ main.tf
@@ -36,3 +36,10 @@ resource "azurerm_public_ip" "main" {
   location            = azurerm_resource_group.main.location
   allocation_method   = "Dynamic"
 }
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.labelPrefix}-A05-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
